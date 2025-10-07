a9c0de8251e86fe6d0ea1c426b2187b3628211f8
hapiccolo <hapiccolo86@gmail.com>
2025-10-07 13:06:31 -0300

    Agregado ubicacion

--- index.php
+++ index.php
@@ -115,6 +115,15 @@ include "head.php";
         </div>
     </section>
 
+    <section id="ubicacion">
+        <h1>ENCONTRANOS</h1>
+        <p>Visitanos en nuestra cafetería, te esperamos con el mejor café y un ambiente cálido.</p>
+        <br>
+        <div style="border:0; width:100%; height:400px;">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1353.8461855310036!2d-58.83727683620389!3d-27.4654047690316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sar!4v1759853049170!5m2!1ses-419!2sar" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </section>
+
     <!-- importacion del footer -->
     <?php
     include "footer.php";
